80d2f3a91efa9dea8ae4c626be3ca41c6f46bb88
André Schreck <moin@andreschreck.de>
2022-01-11 22:09:52 +0100

    typo

--- backend/src/main/java/com/example/demo/TestController.java
+++ backend/src/main/java/com/example/demo/TestController.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import org.springframework.web.bind.annotation.GetMaping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
