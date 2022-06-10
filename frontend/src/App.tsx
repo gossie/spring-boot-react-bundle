e592e72a486698a9e4377a49e48fb1b49c8fd926
Lukas <lukasduechting@mailbox.org>
2022-06-10 11:56:09 +0200

    resolve comments on old pull requests, make react frontend generally work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-    const [greeting, setGreeting] = useState('')
-
-    useEffect(() => {
-        fetch('/api/greeting', {
-            method: 'GET',
-            headers: {
-                'Accept': 'text/plain'
-            }
-        })
-            .then(response => response.text())
-            .then(text => setGreeting(text))
-            .catch(err => setGreeting('Da ist etwas schief gelaufen'));
-    }, []);
-
     return (
         <div>
-            {greeting}
+            Hallo
         </div>
     );
 }
