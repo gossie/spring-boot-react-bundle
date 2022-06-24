afed968a6aa4a42012e499a10e568d39f81fdd6c
chris <chrisweber1991@gmail.com>
2022-06-24 15:11:30 +0200

    localhost

--- frontend/src/service/apiServices.ts
+++ frontend/src/service/apiServices.ts
@@ -2,28 +2,28 @@ import axios, { AxiosResponse } from "axios";
 import { Task } from "./model";
 
 export function fetchAllTasks() {
-    return axios.get('http://localhost:8080/api/kanban')
+    return axios.get('/api/kanban')
         .then((response: AxiosResponse<Array<Task>, any> ) => response.data)
 }
 
 export function createTask(task: Task) {
-    return axios.post('http://localhost:8080/api/kanban', task)
+    return axios.post('/api/kanban', task)
 }
 
 export function deleteTask(taskId: string) {
-    return axios.delete(`http://localhost:8080/api/kanban/` + taskId)
+    return axios.delete(`/api/kanban/` + taskId)
 }
 
 export function promoteTask(task: Task){
-    return axios.put(`http://localhost:8080/api/kanban/next`, task)
+    return axios.put(`/api/kanban/next`, task)
 }
 
 export function demoteTask(task: Task){
-    return axios.put(`http://localhost:8080/api/kanban/prev`, task)
+    return axios.put(`/api/kanban/prev`, task)
 }
 
 export function editTask(task: Task){
-    return axios.put(`http://localhost:8080/api/kanban/`, task)
+    return axios.put(`/api/kanban/`, task)
 }
 
 export function getTask(taskId: string){
