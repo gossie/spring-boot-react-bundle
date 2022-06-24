4943f3201dd73158183d0b7bc687aefd16c7c881
chris <chrisweber1991@gmail.com>
2022-06-24 16:06:56 +0200

    bug fixes

--- backend/src/main/java/com/example/demo/Task.java
+++ backend/src/main/java/com/example/demo/Task.java
@@ -2,20 +2,24 @@ package com.example.demo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "tasks")
 public class Task {
 
+    @Id
     private  String id;
     private  String task;
     private  String description;
     private  EnumStatus status = EnumStatus.OPEN;
 
-    public Task(@JsonProperty String task, String discription) {
+    public Task(String task, String discription) {
         this.task = task;
         this.description = discription;
         id = UUID.randomUUID().toString();
