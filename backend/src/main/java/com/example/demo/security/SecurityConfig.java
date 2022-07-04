cc89da5b9c6c739d5360b73fc0cf7043b2735941
Lukas <lukasduechting@mailbox.org>
2022-07-04 09:18:23 +0200

    Endpunkte absichern in SecurityConfig

--- backend/src/main/java/com/example/demo/security/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,5 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
     }
 }
